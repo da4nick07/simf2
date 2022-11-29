5cabbc9c17c01e2614b4d02e89e35fa962b26957
da4nick07 <vlevkovsky@mail.ru>
2022-11-29 17:05:33 +0300

    Полировка

--- src/CompilerPass/TestPass.php
+++ src/CompilerPass/TestPass.php
@@ -9,15 +9,21 @@ class TestPass  implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
-        if (! $this->isPHPUnit()) {
-            return;
-        }
-        // если "в лоб"
-//        $container->getDefinition(SpamChecker::class)->setPublic(true);
+//        if (! $this->isPHPUnit()) {
+//            return;
+//        }
 
-        // по тегу
-        foreach ($container->findTaggedServiceIds('tested.services') as $id => $tags) {
-            $container->getDefinition($id)->setPublic(true);
+        // так, наверное, правильнее
+        // если не хочется всё писать в config/services_test.yaml
+        if ( $_ENV['APP_ENV'] ==='test') {
+
+            // если "в лоб"
+//          $container->getDefinition(SpamChecker::class)->setPublic(true);
+
+            // по тегу
+            foreach ($container->findTaggedServiceIds('tested.services') as $id => $tags) {
+                $container->getDefinition($id)->setPublic(true);
+            }
         }
     }
 
